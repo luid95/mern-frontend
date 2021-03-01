64118e0358e2debd58e88a6981a65b410d70e64e
luid95 <luisdavidmorales95@gmail.com>
2021-03-01 14:58:02 -0600

    first commit

---
+++ src/core/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (res, req) => {
+    return(
+        <div>
+            <h2>This is de Home Component</h2>
+        </div>
+    );
+}
+
+export default Home;
